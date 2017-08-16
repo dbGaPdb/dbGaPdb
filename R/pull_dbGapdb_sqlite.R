5fecd10da97b7dbf2b4031b67c0080d4d8b0d6d2
Ubuntu <ubuntu@ip-172-31-81-208.ec2.internal>
2017-08-16 18:15:53 +0000

    overwrite downloaded dbGaPdb file on gzip

--- R/pull_dbGapdb_sqlite.R
+++ R/pull_dbGapdb_sqlite.R
@@ -5,10 +5,10 @@
 pull_dbGaPdb_sqlite <- function(local_path=tempdir(),
                                 sqlite_db_name='dbGaPdb.sqlite',
                                 link='https://helix.nih.gov/~mcgaugheyd/dbGapdb.sqlite.gz'){
-  path_and_file_gz <- paste0(local_path, '/', sqlite_db_name, '.gz')
-  path_and_file <- paste0(local_path, '/', sqlite_db_name)
+  path_and_file <- file.path(local_path, sqlite_db_name)
+  path_and_file_gz <- paste0(path_and_file,'.gz')
   download.file(link, path_and_file_gz)
-  print('Downloaded, now unzipping')
-  R.utils::gunzip(path_and_file_gz)
+  message('Downloaded, now unzipping')
+  R.utils::gunzip(path_and_file_gz,overwrite=TRUE)
   return(path_and_file)
 }
